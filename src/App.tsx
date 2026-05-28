604bd67f2aa10b97cb34067a021d6a6459dfd2ca
Andre Gonzalez <gonzalez.andreluiz@gmail.com>
2026-05-28 19:46:41 -0300

    fix: redirect / to /form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { FormPage } from './pages/FormPage'
 import { AdminPage } from './pages/AdminPage'
 import { NotFoundPage } from './pages/NotFoundPage'
@@ -7,6 +7,7 @@ export function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/form" replace />} />
         <Route path="/form" element={<FormPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="*" element={<NotFoundPage />} />
